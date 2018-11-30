665fd1056745a1929d1c7bc8f49e06e5514a8d1a
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2018-11-30 11:56:25 +0530

    Update ComputeAreaWithConstant.java

--- ComputeAreaWithConstant.java
+++ ComputeAreaWithConstant.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ComputeAreaWithConstant{
 	public static void main(String [] args){
-		final double PI = 3.14259;
+		final double PI = 3.14159;
 
 		Scanner input = new Scanner(System.in);
 
